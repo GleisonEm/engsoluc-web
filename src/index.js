b35f413f60f19c97123a80df08dc67dfa0dfd21a
gleison1208 <gleison.spfc1208@hotmail.com>
2020-10-26 10:15:33 -0300

    index src

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import SignIn from './pages/#signin/sign-in';
+import Routes from './services/routes';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   <Routes/>
   </React.StrictMode>,
   document.getElementById('root')
 );
